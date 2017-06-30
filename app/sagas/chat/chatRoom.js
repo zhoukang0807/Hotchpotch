3a5c07386f34366223f8516554317a638eb270bf
Administrator <15251895180@163.com>
2017-06-30 17:11:53 +0800

    调试

---
+++ app/sagas/chat/chatRoom.js
@@ -0,0 +1,37 @@
+import { put, take, call, fork } from 'redux-saga/effects';
+import * as types from '../../constants/ActionTypes';
+import { request,enterWebScoket } from '../../utils/RequestUtil';
+import { USER_LOGIN } from '../../constants/Urls';
+import {  fetchLogin,receiveLogin } from '../../actions/user/login';
+import store from 'react-native-simple-store';
+import { toastShort } from '../../utils/ToastUtil';
+export function* requestLogin(userName,password) {
+    try {
+        enterWebScoket("adas*12","rid","username");
+        yield put(fetchLogin());
+        const loginInfo = yield call(request, USER_LOGIN, 'post', JSON.stringify({userName, password}));
+        console.log(loginInfo)
+        yield put(receiveLogin(loginInfo));
+        yield call(store.save, 'loginInfo', loginInfo); //将数据存储到store中
+        if (loginInfo.resultCode != "0000") {
+            yield toastShort(loginInfo.resultDesc); //toastShort安卓内提示用。提示错误信息
+        }
+    } catch (error) {
+        console.log(error)
+        yield put(receiveLogin(null));
+        yield toastShort(error); //toastShort安卓内提示用。提示错误信息
+    }
+}
+
+export function* watchRequestLogin() {
+    while (true) {
+        const { userName, password} = yield take(
+            types.REQUEST_LOGIN
+        );
+        yield fork(
+            requestLogin,
+            userName,
+            password
+        );
+    }
+}
--- app/utils/RequestUtil.js
+++ app/utils/RequestUtil.js
@@ -1,3 +1,4 @@
+import Pomelo from 'react-native-pomelo';
 const getUrl = (url) => {
   if (url.indexOf('?') === -1) {
     return `${url}?showapi_appid=29400&showapi_sign=e7977541307547beab3e4aa033adb78f`;
@@ -34,4 +35,45 @@ export const request = (url, method, body) => {
         reject(error);
       });
   });
+};
+
+export const enterWebScoket = (uid="", rid="", username="") => {
+    return new Promise((resolve, reject) => {
+        Pomelo.init({
+            host: "169.254.108.40",
+            port: 3014,
+            log: true
+        }, function() {
+            Pomelo.request('gate.gateHandler.queryEntry', {
+                uid: uid
+            }, function(data) {
+                Pomelo.disconnect();
+                Pomelo.init({
+                    host: data.host,
+                    port: data.port,
+                    log: true
+                }, function() {
+                    Pomelo.request("connector.entryHandler.enter", {
+                        username: username,
+                        rid: rid
+                    }, function(data) {
+                        chatSend();
+                    });
+                });
+            });
+        });
+        function chatSend() {
+            let target = "*";
+            let msg = "msg"
+            Pomelo.request("chat.chatHandler.send", {
+                rid: rid,
+                content: msg,
+                from: username,
+                target: target
+            }, function(data) {
+                console.log( data );
+                resolve(data);
+            });
+        }
+    });
 };
