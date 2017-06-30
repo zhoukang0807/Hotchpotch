0524b7dd344568f26d6df61e4d7d0b0d3721e35c
Administrator <15251895180@163.com>
2017-06-30 10:45:11 +0800

    调试

--- app/pages/Read.js
+++ app/pages/Read.js
@@ -1,8 +1,19 @@
 /**
  * Created by PC on 2017/6/28.
  */
-import React, { PropTypes } from 'react';
-import { StyleSheet,ScrollView, Image,ListView ,Text, Linking, View ,RefreshControl,TouchableHighlight,TouchableOpacity} from 'react-native';
+import React, {PropTypes} from 'react';
+import {
+    StyleSheet,
+    ScrollView,
+    Image,
+    ListView,
+    Text,
+    Linking,
+    View,
+    RefreshControl,
+    TouchableHighlight,
+    TouchableOpacity
+} from 'react-native';
 import GridView from '../components/GridView';
 
 import Button from '../components/Button';
@@ -18,20 +29,21 @@ const contextTypes = {
 class Read extends React.Component {
     constructor(props) {
         super(props);
-        const { readActions } = this.props;
+        const {readActions} = this.props;
         readActions.requestArticleList();
         console.log(33);
 
     }
+
     renderListView() {
 
-        const { read } = this.props;
+        const {read} = this.props;
         var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
             dataSource: ds.cloneWithRows(read.articleList),
         };
         console.log(read.articleList);
-        if(read.articleList){
+        if (read.articleList) {
             return (
                 <ScrollView
                     automaticallyAdjustContentInsets={false}
@@ -50,28 +62,33 @@ class Read extends React.Component {
                     <View style={styles.gridLayout}>
                         <ListView
                             dataSource={this.state.dataSource}
-                            renderRow={(rowData,sectionID,rowID) =>
-                            <View >
-                            <TouchableOpacity
-                                underlayColor="rgb(210, 230,255)"
-                                activeOpacity={0.5}
-                                style={{ borderRadius: 8,padding:6,marginTop:5}} >
-                                <View>
-                                    <View style={styles.row}>
-                                        <Image style={styles.thumb} source={require('../img/about_logo.png')} />
-                                        <Text style={{flex:1,fontSize:16,color:'blue',borderBottomColor:'red'}}>{rowID}.{rowData.title}</Text>
-                                    </View>
-                                    <View style={styles.separator} />
+                            renderRow={(rowData, sectionID, rowID) =>
+                                <View >
+                                    <TouchableOpacity
+                                        underlayColor="rgb(210, 230,255)"
+                                        activeOpacity={0.5}
+                                        style={{borderRadius: 8, padding: 6, marginTop: 5}}>
+                                        <View>
+                                            <View style={styles.row}>
+                                                <Image style={styles.thumb} source={require('../img/about_logo.png')}/>
+                                                <Text style={{
+                                                    flex: 1,
+                                                    fontSize: 16,
+                                                    color: 'blue',
+                                                    borderBottomColor: 'red'
+                                                }}>{rowID}.{rowData.title}</Text>
+                                            </View>
+                                            <View style={styles.separator}/>
+                                        </View>
+                                    </TouchableOpacity>
                                 </View>
-                            </TouchableOpacity>
-                                </View>
-                                }
+                            }
                         />
 
                     </View>
                 </ScrollView>
             );
-        }else{
+        } else {
             return (
                 <View >
                     <Text>没有获取到文章列表！</Text>
@@ -81,6 +98,7 @@ class Read extends React.Component {
         }
 
     }
+
     render() {
         return (
             <View style={styles.container}>
@@ -88,7 +106,7 @@ class Read extends React.Component {
                     <Text
                         style={[
                             styles.btnText,
-                            { color: 'black',align:'center', padding: 5, fontSize: 18 }
+                            {color: 'black', align: 'center', padding: 5, fontSize: 18}
                         ]}
                     >
                         read界面
@@ -110,29 +128,26 @@ const styles = StyleSheet.create({
         fontSize: 14,
         textAlign: 'center'
     },
-    textStyle:{
-        borderBottom:"#000000"
-    },
-row: {
-    flexDirection: 'row',
+    row: {
+        flexDirection: 'row',
         justifyContent: 'center',
         padding: 10,
         backgroundColor: '#F6F6F6',
-},
-separator: {
-    height: 1,
+    },
+    separator: {
+        height: 1,
         backgroundColor: '#CCCCCC',
-},
-thumb: {
-    width: 64,
+    },
+    thumb: {
+        width: 64,
         height: 64,
-},
-text: {
-    flex: 1,
+    },
+    text: {
+        flex: 1,
         alignItems: 'center',    //#水平居中
         justifyContent: 'center',//  #垂直居中
         textAlign: 'center',  // #文字水平居中
-},
+    },
 });
 
 export default Read;
